84ccde8f144eef1fcd0a56d3295fa613a1da2858
Isaac Miti <ikayz@live.com>
2023-01-04 17:08:12 +0200

    fix image having padding

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,15 +5,17 @@ function Information() {
   return (
     <header className="header">
       <img src={profile} width="250" alt="My profile picture" />
-      <h1>Isaac Miti</h1>
-      <h2>Frontend Developer</h2>
-      <p>ikayz.dev</p>
-      <button type="button" className="email-btn">
-        <i className="fa fa-envelope"></i> Email
-      </button>
-      <button type="button" className="linkedin-btn">
-        <i className="fa fa-linkedin-square"></i> LinkedIn
-      </button>
+      <div className="inner-header">
+        <h1>Isaac Miti</h1>
+        <h2>Frontend Developer</h2>
+        <p>ikayz.dev</p>
+        <button type="button" className="email-btn">
+          <i className="fa fa-envelope"></i> Email
+        </button>
+        <button type="button" className="linkedin-btn">
+          <i className="fa fa-linkedin-square"></i> LinkedIn
+        </button>
+      </div>
     </header>
   );
 }
